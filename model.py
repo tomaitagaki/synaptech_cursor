d75df9fc519dd61798615a647340d8051794585b
tomaitagaki <45250757+tomaitagaki@users.noreply.github.com>
2022-10-10 17:45:40 -0700

    Create model.py

---
+++ model.py
@@ -0,0 +1,36 @@
+"""
+The model handles calculations, updating the model, etc.
+"""
+
+
+class Model():
+
+    def __init__(self, ...):
+        pass
+
+    def _get_model(self, ...):
+        pass
+
+    # if needed to
+    def normalize(self, ...):
+        pass
+
+    def update(self, ...):
+        pass
+
+    def calculate_loss(self, ...):
+        pass
+
+    def save_model(self, ...):
+        pass
+
+    def predict(self, ...):
+        pass
+
+# generate target position
+
+# convert features to movement
+
+# __calculate error 
+
+# update model
